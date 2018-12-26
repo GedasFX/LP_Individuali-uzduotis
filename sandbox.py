62265651fe2726a4d828a71eb8808b943f373637
Gediminas <8672277+GedasFX@users.noreply.github.com>
2018-12-26 22:33:39 +0200

    Working initial copy

--- sandbox.py
+++ sandbox.py
@@ -1,3 +1,5 @@
+from multiprocessing import Process, Queue
+
 class Data:
     def __init__(self, element, amount):
         self.element = element
@@ -7,7 +9,26 @@ class Data:
     def __repr__(self):
         return self.__str__()
 
-list = [None] * 100
-print(list)
-list.append(Data("Vienas", 1))
-print(list)
+def add(q):
+    print("a")
+    d = q.get()
+    print("b")
+    q.put(d)
+    print("c")
+    d = q.get()
+    print("d")
+    q.put(d)
+
+if __name__ == '__main__':
+    q = Queue()
+    a = []
+    print(1)
+    p = Process(target = add, args = (q,))
+    print(2)
+    p.start()
+    print(3)
+    q.put(0)
+    p.join()
+    print(4)
+    print(q.get())
+    print(len(a))
--- source.py
+++ source.py
@@ -1,5 +1,7 @@
 # Individuali Užduotis IFF-6/2 Gediminas Milašius
 
+from multiprocessing import Process, Queue
+
 # Contains 2 fields: Element of any type and an integer count
 class Data:
     def __init__(self, element, count):
@@ -7,6 +9,16 @@ class Data:
         self.count = count
     def __str__(self):
         return self.element + ": " + str(self.count)
+    def __repr__(self):
+        return self.__str__()
+
+# Identifies data to be written or read and provides the element
+class Pass:
+    def __init__(self, element, read):
+        self.element = element
+        self.read = read
+    def __str__(self):
+        return "Remove " + self.element
     def __repr__(self):
         return self.__str__()
 
@@ -69,7 +81,7 @@ class DataMonitor:
 
         # Array is full and/or element doesn't exist
         return False
-# ==== Methods for modifying array _data and field _count ====
+# ==== Methods for modifying data array ====
 
 # Read the file and output it as 3d array: [W/R index][Group Index][Element Index]
 def readFile(path):
@@ -87,10 +99,93 @@ def readFile(path):
     file.close()
     return result
 
-# Main function
-def main():
-    rawData = readFile("IFF-6-2_MilasiusG_IP_dat_14.txt")
-    print(rawData)
+# ==== Parallel processes ====
+# Manager process. Has access to the data structure. Is given the amount of processes to do the job
+def manager(qData, qLog, qRes, cWorkers):
+    completedCnt    = 0     # Worker processes, which have finished work
+    unfinishedBuf   = []    # Overflow buffer to be dealt with after all elements have been added.
+    data = DataMonitor()
+    # While changes are possible
+    while completedCnt < cWorkers:
+        el = qData.get()
+        if el is None:
+            completedCnt = completedCnt + 1
+            continue
 
-# Run the program
-main()
+        if el.read:
+            if data.remove(el.element):
+                qLog.put("Successfully removed \"" + el.element + "\".")
+            else:       
+                unfinishedBuf.append(el.element)
+                qLog.put("Failed to remove \"" + el.element + "\". Will retry later.")
+        else:
+            data.add(el.element)
+            qLog.put("Successfully added \"" + el.element + "\".")
+    # At this point no new elements can be added to the array, only removed.
+    # Program can continue exectution in serial as input is ignored.
+    for element in unfinishedBuf:
+        if data.remove(element):
+            qLog.put("Successfully removed \"" + element + "\".")
+        else:
+            qLog.put("Invalid input file. Duplicate \"" + element + "\" detected. Element will be ignored")
+
+    # Array is final, return to main thread
+    qRes.put(data.count) # Elements in array remaining
+    for el in data.data:
+        if el is None:
+            break
+        qRes.put(el)
+    qLog.put(None) # Stop logger
+
+# Logger process. Logs manager actions
+def logger(q):
+    while True:
+        line = q.get()
+        if line == None:
+            break
+        else:
+            print(line)
+
+# Worker process. Writes/Reads messages to the manager process. Has initial data to provide. Type defines the type: False - writer, True - reader
+def worker(q, dat, read):
+    for entry in dat:
+        for _ in range(entry.count):
+            q.put(Pass(entry.element, read))
+    q.put(None)
+# ==== Parallel processes ====
+
+# Starts processes with correct initial conditions
+def startProcesses(processes, rawData, qData):
+    processes[0].start()
+    processes[1].start()
+
+    for group in rawData[0]:
+        processes[2].append(Process(target = worker, args = (qData, group, False))) # Writers
+    for group in rawData[1]:
+        processes[3].append(Process(target = worker, args = (qData, group, True))) # Readers
+
+    for process in processes[3]:
+        process.start()
+    for process in processes[2]:
+        process.start()
+
+# Main function
+if __name__ == "__main__":
+    rawData = readFile("IFF-6-2_MilasiusG_IP_dat_14.txt")
+    # Data transfer queues. Many2One qData
+    qData = Queue()
+    qResult = Queue()
+    qLog = Queue()
+
+    # Create and start processes
+    processes = [ # Manager process [0], logger process [1], an array of write processes [2], and an array of read ones [3] + len(rawData[1])
+        Process(target = manager, args = (qData, qLog, qResult, len(rawData[0]) + len(rawData[1]),)), # Ambiguous variable = worker count
+        Process(target = logger, args = (qLog,)), [], []
+    ] 
+    startProcesses(processes, rawData, qData)
+
+    # Get final result of the data streams
+    resArr = []
+    count = qResult.get() # First result is quantity
+    for i in range(count):
+        resArr.append(qResult.get())
